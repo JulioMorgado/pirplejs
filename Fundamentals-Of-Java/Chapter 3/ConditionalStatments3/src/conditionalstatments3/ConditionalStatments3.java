8f0bb942d7129515960ba19ea098382a707ff8dc
JulioMorgado <78586706+JulioMorgado@users.noreply.github.com>
2022-06-17 09:45:36 -0500

    Add files via upload

---
+++ Fundamentals-Of-Java/Chapter 3/ConditionalStatments3/src/conditionalstatments3/ConditionalStatments3.java
@@ -0,0 +1,24 @@
+
+package conditionalstatments3;
+
+import java.util.*;
+public class ConditionalStatments3 {
+
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Input now: ");
+        String input = reader.next ();
+        
+        if (input.equals("password"))
+        {
+            System.out.println("Yes");
+        }
+        
+        else
+        {
+            System.out.println("No")
+        }
+        
+    }
+    
+}
